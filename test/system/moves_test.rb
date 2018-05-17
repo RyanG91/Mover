53420fc954546bf0e2e559838fd72835da82787b
RyanG91 <ryan.grosser91@gmail.com>
2018-05-18 07:54:26 +1000

    New app

---
+++ test/system/moves_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MovesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit moves_url
+  #
+  #   assert_selector "h1", text: "Move"
+  # end
+end
