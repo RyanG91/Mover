6464a0c890834a711ce79b92a57dd7ea9721bf0f
RyanG91 <ryan.grosser91@gmail.com>
2018-06-04 21:11:14 +1000

    Stripe changes

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,32 +3,36 @@ class ChargesController < ApplicationController
   def new
   end
 
-  def show
-    respond_to do |format|
-      format.json  { render :json => charge }
-      format.html  { render :template => "charges/create"}
-    end
-  end
-
   def create
-    # Amount in cents, this is mainly for testing at the moment
-    @amount = 500
+    @amount = params[:amount]
 
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
+    @amount = @amount.gsub('$', '').gsub(',', '')
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'aud' #or whatever currency you like
+    begin
+      @amount = Float(@amount).round(2)
+    rescue
+      flash[:error] = 'Charge not completed. Please enter a valid amount in USD ($).'
+      redirect_to new_charge_path
+      return
+    end
+
+    @amount = (@amount * 100).to_i # Must be an integer!
+
+    if @amount < 500
+      flash[:error] = 'Charge not completed. Donation amount must be at least $5.'
+      redirect_to new_charge_path
+      return
+    end
+
+    Stripe::Charge.create(
+      :amount => @amount,
+      :currency => 'usd',
+      :source => params[:stripeToken],
+      :description => 'Custom donation'
     )
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_charge_path
+    end
   end
-
-end
