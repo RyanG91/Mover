20fb0a967b44005c95e1eaedb76f188dd0b21aba
RyanG91 <ryan.grosser91@gmail.com>
2018-06-23 19:55:22 +1000

    Geocoder added to provider scaffold

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,10 @@
 class Provider < ApplicationRecord
+
+  geocoded_by :provider_geocode   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
+
+  def provider_geocode
+    "#{city}, #{state}"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180617110653) do
+ActiveRecord::Schema.define(version: 20180623093503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,8 @@ ActiveRecord::Schema.define(version: 20180617110653) do
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "requests", force: :cascade do |t|
