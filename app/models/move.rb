df3bc6775aa7dcb5f2582d026d4c7c4f5b35a10f
Ryan Grosser <ryan.grosser91@gmail.com>
2018-09-14 09:31:58 +1000

    Added a basic search bar

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,3 +1,8 @@
 class Move < ApplicationRecord
   resourcify
+
+  scope(:title, -> (title) { where("LOWER(title) like ?", "%#{title.downcase}%")})
+  scope(:owner, -> (owner) { where("LOWER(owner) like ?", "%#{owner.downcase}%")})
+  scope(:cost, -> (cost) { where cost: cost })
+
 end
